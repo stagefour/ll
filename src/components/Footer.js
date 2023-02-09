6e00906237d2dfaf8f454e6595e7aa08ed677a36
Pawel Banasiewicz <xghoul@gmail.com>
2023-02-09 10:09:41 +0100

    little lemon repository

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+import './Footer.css';
+
+
+export default function Footer () {
+    return(
+        <>
+            <div className="footer">
+                <h4>(c) 2023 Coursera Learner</h4>
+            </div>
+        </>
+    );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import './Main.css';
+
+import {
+    Heading
+  } from "@chakra-ui/react";
+
+
+export default function Main () {
+    return (
+        <>
+        <div className='specialsDiv'>
+            <Heading as="h1">
+                Little Lemon Restaurant<br/>
+                MAIN THING
+            </Heading>
+        </div>
+        </>
+    );
+};
+
